c16f52f155c2dcd566b3938c34fae8b386c31800
Your Name <larkinsjeffry71@gmail.com>
2022-02-19 16:52:31 +0530

    first command

---
+++ utils/all_utils.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import joblib
+from matplotlib.colors import ListedColormap
+import os
+
+plt.style.use("fivethirtyeight")
+
+def prepare_data(df):
+    X = df.drop("y", axis = 1)
+    y = df["y"]
+    return X, y
+
+    ##SAVING THE MODESL
+def save_model(model, filename):
+    model_dir = 'models'
+    os.makedirs(model_dir, exist_ok = True)
+    filePath = os.path.join(model_dir, filename)
+    joblib.dump(model, filePath)
